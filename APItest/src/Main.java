cbbc6fd01c5ab59fb3c6a582dc7466ab65d758a7
Alawon1 <alawon12@gmail.com>
2023-01-05 11:05:11 +0100

    2

--- APItest/src/Main.java
+++ APItest/src/Main.java
@@ -12,9 +12,27 @@ public class Main {
 
         URL api = null;
         try{
-            api = new URL("https://catfact.ninja/fact");
+            api = new URL("https://www.antwiki.org/wiki/images/0/0c/AntWiki_Regional_Taxon_List.txt");
         }catch(Exception e){
             System.out.println("Hello world!");
+            return;
+        }
+        InputStream input = null;
+        try{
+            input = api.openConnection().getInputStream();
+        }
+        catch(Exception e){
+            return;
+        }
+        try(BufferedReader br = new BufferedReader(new InputStreamReader(input))) {
+            String line = null;
+
+            // read each line and write to System.out
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+        }
+        catch (Exception e){
             return;
         }
 
